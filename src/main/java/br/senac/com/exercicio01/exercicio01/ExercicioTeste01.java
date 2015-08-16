34615b52034802f4403951bc648f04490a92cecc
JOSEROBERTO <JOSEROBERTO@192.168.1.104>
2015-08-16 18:02:32 -0300

    Testando - Commit - Github

---
+++ src/main/java/br/senac/com/exercicio01/exercicio01/ExercicioTeste01.java
@@ -0,0 +1,10 @@
+package br.senac.com.exercicio01.exercicio01;
+
+
+public class ExercicioTeste01 {
+    
+    public static void main(String[] args) {
+        System.out.println("teste Git");
+    }
+    
+}
